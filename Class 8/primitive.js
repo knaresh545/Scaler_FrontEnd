d0569d04074cc71c92739da6f5977d26359941e7
knaresh545 <91107057+knaresh545@users.noreply.github.com>
2023-09-26 09:35:32 +0530

    Add files via upload

---
+++ Class 8/primitive.js
@@ -0,0 +1,46 @@
+// Number datatype
+
+// let a = 6;
+// let b = 9.0;
+// let c = -1000;
+// console.log(a, b, c);
+// console.log(typeof a, typeof b, typeof c);
+
+// String datatype
+
+// let a = "String";
+// let b = "String";
+// let c = `String`;
+
+// Build a string like this
+// Windows 10, Windows 11, Windows X
+// Verion is coming from a varaible called version
+//let version = "X", Windows 10/11/X
+
+// let version = 11;
+// console.log("Windows" + version);
+
+// Template literals
+// console.log(`Windows ${version}`);
+
+// console.log(a, b, c);
+// console.log(typeof a, typeof b, typeof c);
+
+// boolean datatype
+
+// const bool = true;
+// console.log(bool);
+// console.log(typeof bool);
+
+// Undefined and Null datatype
+let a;
+console.log(a);
+console.log(typeof a);
+
+// undefined is not something needed to be specified by the user
+
+let b = null;
+console.log(b);
+console.log(typeof b);
+
+// null always something that has to be specified by the user
